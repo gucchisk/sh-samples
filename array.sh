43be3f574a763a50df8560940c45c8e9f497d728
gucchisk <gucchi_sk@yahoo.co.jp>
2020-07-01 01:25:49 +0900

    clarify

--- array.sh
+++ array.sh
@@ -3,26 +3,15 @@
 # for centos7
 #
 
-echo "[openresty]
-name=Official OpenResty Open Source Repository for CentOS
-baseurl=https://openresty.org/package/centos/$releasever/$basearch
-skip_if_unavailable=False
-gpgcheck=1
-repo_gpgcheck=0
-gpgkey=https://openresty.org/package/pubkey.gpg
-enabled=1
-enabled_metadata=1" > /etc/yum.repos.d/openresty.repo
-
 declare -a packages=(
     "openresty-1.15.8.3-1.el7.x86_64"
-    "openresty-pcre-8.44-1.el7.x86_64"
-    "openresty-zlib-1.2.11-3.el7.centos.x86_64"
+    "python-2.7.5-88.el7.x86_64"
 )
 
 for e in ${packages[@]}; do
-    if [ -n $(repoquery ${e}) ]; then
-	echo "already exist ${e}.rpm"
+    if [ -n "$(repoquery ${e})" ]; then
+	echo "${e}.rpm  ok"
     else
-	echo "no ${e}.rpm"
+	echo "${e}.rpm  no"
     fi
 done
